ff1036a4fb1a16bd050c191037277a75841c5a76
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-04-20 16:28:53 +0500

    In this project i use class method,generate random five number id and use class function.

---
+++ index.ts
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+let condition = true;
+class Student {
+    name:string;
+    id:number;
+    courses:any;
+    balance:number;
+    constructor(name:string,id:number,course:any,balance:number){
+        this.name = name;
+        this.id = id;
+        this.courses = course;
+        this.balance = balance;
+    }
+    show(){
+        console.log(`Student name is ${this.name} \n Student id is ${this.id}\n Student courses is ${this.courses} \n Student balance is ${this.balance}`);
+    }
+}
+
+let GenerateID = Math.floor(Math.random()*100000)+1;
+while(condition){
+let answer = await inquirer.prompt([
+{
+    name : "StudentName",
+    type : "input",
+    message : "Enter the name of Student : "
+},
+{
+    name : "StudentCourses",
+    type : "list",
+    message : "Choose the course which you want to study : ",
+    choices : ["HTML","CSS","Javacript","Typescript"]
+},
+{
+    name : "StudentBalance",
+    type : "number",
+    message : "Enter your balance : "
+},
+{
+    name :"quit",
+    type : "input",
+    message : "Do you want to add another Student Data : (Y/N)"
+}
+]);
+let Student1 = new Student(answer.StudentName,GenerateID,answer.StudentCourses,answer.StudentBalance);
+Student1.show();
+if(answer.quit === "y" || answer.quit === "Y"){
+    condition = true;
+}else{
+    condition = false;
+}
+}
